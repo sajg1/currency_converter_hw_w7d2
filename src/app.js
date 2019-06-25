61a2ae2a944c018879025b43d2389bc61b9c29d9
Steve Gossip <stephengossip@gmail.com>
2019-06-25 17:57:14 +0100

    object exchangeRates is filled using fetch function. li tag on html to print object to screen

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue ({
+    el: "#app",
+    data: {
+      exchangeRates: {}
+    },
+    mounted() {
+      this.getExchangeRates();
+    },
+    methods: {
+      getExchangeRates: function () {
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(exchangeRates => this.exchangeRates = exchangeRates)
+      }
+    }
+  })
+})
